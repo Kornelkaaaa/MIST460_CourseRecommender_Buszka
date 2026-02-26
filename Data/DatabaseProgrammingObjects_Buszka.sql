189419ffd7f625d34e6d5f4ffc5d2d6a589411db
Korneliaaa <kb00145@mix.wvu.edu>
2026-02-25 19:48:47 -0500

    update of programming objects

--- Data/DatabaseProgrammingObjects_Buszka.sql
+++ Data/DatabaseProgrammingObjects_Buszka.sql
@@ -75,63 +75,99 @@ go
 --Output: CourseID, CourseName, PrerequisiteCourseName, MinGradeRequired
 --Conditions: Looks up the course (C) and all prerequisite mappings in CoursePrerequisite (CP), Joins CoursePrerequisite.PrerequisiteID to Course.CourseID to get the full details of each prerequisite course, Filters results to only show prerequisites for the specified @CourseID.
 
-CREATE or alter PROCEDURE GetCoursePrerequisites
-    @CourseID INT
-AS
+CREATE OR ALTER PROCEDURE dbo.GetCoursePrerequisites
+(
+    @CourseID     INT           = NULL,
+    @SubjectCode  VARCHAR(10)   = NULL,
+    @CourseNumber VARCHAR(10)   = NULL
+)
+as
 BEGIN
-    SELECT  c.CourseID,
-            c.SubjectCode + ' ' + c.CourseNumber AS Course,
-            c.Title,
-            prereq.SubjectCode + ' ' + prereq.CourseNumber AS PrerequisiteCourse,
-            prereq.Title AS PrerequisiteTitle,
-            cp.MinGradeRequired
-    FROM    Course as c
-    JOIN    CoursePrerequisite cp    ON c.CourseID = cp.CourseID
-    JOIN    Course prereq      ON cp.PrerequisiteID = prereq.CourseID
-    WHERE   c.CourseID = @CourseID;
+    SELECT prereq.Title, prereq.SubjectCode, prereq.CourseNumber
+    FROM CoursePrerequisite AS cp
+    JOIN Course AS prereq ON cp.PrerequisiteID = prereq.CourseID
+    JOIN Course AS mainCourse ON cp.CourseID = mainCourse.CourseID
+    WHERE mainCourse.SubjectCode = ISNULL(@SubjectCode, mainCourse.SubjectCode)
+    AND mainCourse.CourseNumber = @CourseNumber
 END;
 GO
 
 -- Usage:
--- EXEC GetCoursePrerequisites @CourseID = 4; GO
+-- EXEC GetCoursePrerequisites @SubjectCode = 'MIST', @CourseNumber = '460'; GO
 
+CREATE or alter function fnGetCoursePrerequisites
+(
+    @SubjectCode  VARCHAR(10)   = NULL,
+    @CourseNumber VARCHAR(10)   = NULL
+)
+RETURNS @Prerequisites table
+(
+    Title NVARCHAR(200),
+    SubjectCode NVARCHAR(10),
+    CourseNumber NVARCHAR(10)
+)
+AS
+BEGIN
+    insert into @Prerequisites
+    (Title, SubjectCode, CourseNumber)
+    select prereq.Title, prereq.SubjectCode, prereq.CourseNumber
+    from CoursePrerequisite AS cp
+    JOIN Course AS prereq ON cp.PrerequisiteID = prereq.CourseID
+    JOIN Course AS mainCourse ON cp.CourseID = mainCourse.CourseID
+    WHERE mainCourse.SubjectCode = ISNULL(@SubjectCode, mainCourse.SubjectCode)
+    AND mainCourse.CourseNumber = @CourseNumber
+
+    return;
+END;
+GO
+-- Usage:
+-- SELECT * FROM dbo.fnGetCoursePrerequisites('MIST', '460');GO
 --3. Has spceific student completed the prerequisites for a specific course?
 -- We need small module ?so less line maybe other store pocedures which can we use in future
-CREATE OR ALTER PROCEDURE CheckStudentPrerequisites
-    @StudentID  INT,
-    @CourseID   INT
-AS
+
+-- Find all the courses that spectied student has taken
+create or alter FUNCTION fnGetStudentCompletedCourses
+(
+    @StudentID int
+)
+returns @CourseHistory table
+(
+    SubjectCode nvarchar(10),
+    CourseNumber nvarchar(10),
+    Grade nchar(2)
+)
+as 
 BEGIN
-    SELECT  
-        prereq.SubjectCode + ' ' + prereq.CourseNumber AS PrerequisiteCourse,
-        prereq.Title                                   AS PrerequisiteTitle,
-        cp.MinGradeRequired                                    AS RequiredMinGrade,
-        rs.LetterGrade                                 AS StudentGrade,
-        CASE 
-            WHEN rs.LetterGrade IS NULL THEN 'Not Completed'
-            WHEN rs.LetterGrade IN ('A', 'B', 'C', 'D', 'F') 
-             AND rs.LetterGrade <= cp.MinGradeRequired          THEN 'Completed'
-            ELSE 'Not Completed'
-        END AS PrerequisiteStatus
+    insert into @CourseHistory
+    (SubjectCode, CourseNumber, Grade)
+    select 
+        C.SubjectCode, 
+        C.CourseNumber, 
+        rs.LetterGrade
+    from Registration R
+    join RegistrationSection rs on R.RegistrationID = rs.RegistrationID
+    join Section S on rs.SectionID = S.SectionID
+    join Course C on S.CourseID = C.CourseID
+    where R.StudentID = @StudentID;
 
-    FROM CoursePrerequisite as cp
-    --The Course table is joined TWICE — once for the main course, once for the prerequisite:
-    JOIN Course as prereq  ON cp.PrerequisiteID = prereq.CourseID
-
-    -- Find the section for the prereq course
-    LEFT JOIN Section as s  ON s.CourseID = prereq.CourseID
-
-    -- Find if the student was enrolled in that section
-    LEFT JOIN RegistrationSection as rs  ON rs.SectionID = s.SectionID AND rs.EnrollmentStatus = 'Completed'
-
-    -- Link back to the student
-    LEFT JOIN Registration as r  ON rs.RegistrationID = r.RegistrationID AND r.StudentID = @StudentID
-
-    WHERE cp.CourseID = @CourseID;
+    return;
 END;
 GO
-
 -- Usage:
--- EXEC CheckStudentPrerequisites @StudentID = 1, @CourseID = 2
+-- select * from fnGetStudentCompletedCourses(@StudentID = 1);
 
+-- DECLARE @firstGrade nchar(2), @secondGrade nchar(2);
+-- set @firstGrade = 'B';
+-- set @secondGrade = 'C';
+-- print(ASCII(@firstGrade));
+-- print(ASCII(@secondGrade));
+-- if(@firstGrade >= @secondGrade)
+--     print "First grade is higher or equal to second grade";
+-- else
+--     print "First grade is lower than second grade"; 
+-- go
+ 
+ --Has specific student completed all prerequisites for a specific course?
+ -- just make a lot of details and use other store procedure
 
+ --azure account. set up an azure sql servier database, create me as user -> run objects, setects
